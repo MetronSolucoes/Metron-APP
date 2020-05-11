6289929e9653b0f261e6041992fa6915d08b4fd9
giovani.zaparoli <giovani.zaparoli@leavening.com>
2020-05-11 19:56:36 -0300

    Login page

--- src/App.tsx
+++ src/App.tsx
@@ -34,6 +34,8 @@ import '@ionic/react/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import './assets/css/App.css';
+
 const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -6,7 +6,12 @@ import { IonContent,
          IonLabel,
          IonGrid,
          IonRow,
+         IonImg,
+         IonThumbnail,
+         IonButton,
+         IonText,
          IonCol } from '@ionic/react';
+
 
 const Login: React.FC = () => {
   return (
@@ -14,12 +19,31 @@ const Login: React.FC = () => {
       <IonContent>
         <IonGrid>
           <IonRow>
-            <IonCol size="12">
-              <h4>Login</h4>
+            <IonCol size="12" class="d-flex justify-content-center">
               <IonItem>
-                <IonLabel position="floating">E-mail</IonLabel>
-                <IonInput type="email" mode="md"></IonInput>
+                <IonImg class="img-size" src="assets/icon/metron.jpg" />
               </IonItem>
+            </IonCol>
+            <IonCol size="12">
+              <IonText class="text-center">
+                <h2>Login</h2>
+              </IonText>
+              <IonItem>
+                <IonLabel position="floating" class="pl-2 input-text-color">E-mail</IonLabel>
+                <IonInput type="email" mode="md" class="bordered-input pl-2" inputmode="email"></IonInput>
+              </IonItem>
+            </IonCol>
+            <IonCol size="12">
+              <IonItem>
+                <IonLabel position="floating" class="pl-2 input-text-color">Senha</IonLabel>
+                <IonInput type="password" mode="md" class="bordered-input pl-2"></IonInput>
+              </IonItem>
+            </IonCol>
+            <IonCol size="12" class="px-3">
+              <IonButton color="primary" mode="md" expand="block">Entrar</IonButton>
+            </IonCol>
+            <IonCol size="12" class="px-3">
+              <IonButton color="primary" mode="md" expand="block" fill="outline">Esqueceu sua senha?</IonButton>
             </IonCol>
           </IonRow>
         </IonGrid>
